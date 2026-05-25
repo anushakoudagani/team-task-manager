238ce7d90537b00008af35ce4af377f9c33ccb8b
Anusha koudagani <anushakoudagani6@gmail.com>
2026-05-25 11:36:40 +0530

    fixed server

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.get("/", (req, res) => {
+  res.send("Server is running successfully 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
