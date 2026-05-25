760d8a4ae7779900e88a05eab586049528a52bd0
Anusha koudagani <anushakoudagani6@gmail.com>
2026-05-25 13:23:39 +0530

    serve frontend

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,15 @@
 const express = require("express");
-const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("/", (req, res) => {
-  res.send("Server is running successfully 🚀");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
