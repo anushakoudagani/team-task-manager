38b3badede5e9313c3b86d215240f116c4a06ad3
Anusha koudagani <anushakoudagani6@gmail.com>
2026-05-26 22:48:28 +0530

    fix express route

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
