164116dd732d393ec786a4877fa0350705d49354
Anusha koudagani <anushakoudagani6@gmail.com>
2026-05-26 17:50:38 +0530

    final frontend fix

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,12 @@
 const express = require("express");
-const cors = require("cors");
 const path = require("path");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
-app.use(
-  express.static(
-    path.join(__dirname, "../frontend/build")
-  )
-);
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(
-    path.join(
-      __dirname,
-      "../frontend/build/index.html"
-    )
-  );
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
 const PORT = process.env.PORT || 8080;
