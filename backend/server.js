7df07adda535e0aa2da3cb57ebdfca38abf93d0e
Anusha koudagani <anushakoudagani6@gmail.com>
2026-05-26 17:42:41 +0530

    final working railway deploy

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,25 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-console.log(path.join(__dirname, "../frontend/build"));
+app.use(cors());
+app.use(express.json());
+
+app.use(
+  express.static(
+    path.join(__dirname, "../frontend/build")
+  )
+);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  res.sendFile(
+    path.join(
+      __dirname,
+      "../frontend/build/index.html"
+    )
+  );
 });
 
 const PORT = process.env.PORT || 8080;
